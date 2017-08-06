8e56abff370c60d0339cf85b7dede186d5e9a195
Mohammad Amir Jamil <amirjamil900@gmail.com>
2017-08-06 22:19:00 +0300

    Generalise the tag addition for multiple files

--- scripts/setgps.py
+++ scripts/setgps.py
@@ -4,23 +4,40 @@ import sys
 
 usage = """Usage: setgps.py filename.jpg lat lng"""
 
-if len(sys.argv) != 4:
-    print >> sys.stderr, usage
-    sys.exit(1)
+latitude = 0
+longitude= 0
+name="amir.jpg"
+if len(sys.argv)==1:
+    
+    try:
+        ef = JpegFile.fromFile(name)
+        ef.set_geo(latitude,longitude)
+    except IOError:
+        type, value, traceback = sys.exc_info()
+        print >> sys.stderr, "Error opening file:", value
+    except JpegFile.InvalidFile:
+        type, value, traceback = sys.exc_info()
+        print >> sys.stderr, "Error opening file:", value
+    try:
+        ef.writeFile(name)
+    except IOError:
+        type, value, traceback = sys.exc_info()
+        print >> sys.stderr, "Error saving file:", value
+else:
 
-try:
-    ef = JpegFile.fromFile(sys.argv[1])
-    ef.set_geo(float(sys.argv[2]), float(sys.argv[3]))
-except IOError:
-    type, value, traceback = sys.exc_info()
-    print >> sys.stderr, "Error opening file:", value
-except JpegFile.InvalidFile:
-    type, value, traceback = sys.exc_info()
-    print >> sys.stderr, "Error opening file:", value
+    try:
+        ef = JpegFile.fromFile(sys.argv[1])
+        ef.set_geo(float(sys.argv[2]), float(sys.argv[3]))
+    except IOError:
+        type, value, traceback = sys.exc_info()
+        print >> sys.stderr, "Error opening file:", value
+    except JpegFile.InvalidFile:
+        type, value, traceback = sys.exc_info()
+        print >> sys.stderr, "Error opening file:", value
 
-try:
-    ef.writeFile(sys.argv[1])
-except IOError:
-    type, value, traceback = sys.exc_info()
-    print >> sys.stderr, "Error saving file:", value
-
+    try:
+        ef.writeFile(sys.argv[1])
+    except IOError:
+        type, value, traceback = sys.exc_info()
+        print >> sys.stderr, "Error saving file:", value
+sys.exit(1)
